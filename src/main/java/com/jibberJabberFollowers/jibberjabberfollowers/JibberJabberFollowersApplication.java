256bfec741d548ccafb2919bb60996cb8a634a97
Nico-Werner <nicolas.werner@ing.austral.edu.ar>
2022-06-04 20:06:59 -0300

    First commit

---
+++ src/main/java/com/jibberJabberFollowers/jibberjabberfollowers/JibberJabberFollowersApplication.java
@@ -0,0 +1,13 @@
+package com.jibberJabberFollowers.jibberjabberfollowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberFollowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberFollowersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jibberJabberFollowers/jibberjabberfollowers/JibberJabberFollowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibberJabberFollowers.jibberjabberfollowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberFollowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
